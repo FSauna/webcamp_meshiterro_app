9d099ee166061e8de3d0c389c1c3d19dd75dd996
FSauna <yfuta0630@gmail.com>
2021-09-20 07:25:40 +0000

    [Add] meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '597a5217743be742f2fb6637bf599a88e2b1d2e085edc1ce55e709d7e60401f97db6595e468dbaaffc77a43d4de40c048303e5dbf83a10c93ef83f9f9eb94931'
